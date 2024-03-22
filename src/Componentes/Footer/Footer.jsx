dae2703cdf81e0f63a8b116f065f40a65e0009b8
JenniGabriely01 <141264193+JenniGabriely01@users.noreply.github.com>
2024-03-22 16:45:12 -0300

    Estilizando

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -7,22 +7,26 @@ export function Footer() {
 
                 <hr />
                 <div className="footer">
-                    
+
                     <div className="conteudo-direita">
                         <img src="/imagens/Logo.svg" alt="Logo" />
                         <p>Todos os direitos reservados</p>
-                        
-                            <img src="/icones/instagram.svg" alt="icone-instagram" />
-                            <img src="/icones/face.svg" alt="icone-facebook" />
-                            <img src="/icones/wpp.svg" alt="icone-whatsapp" />
+
+                        <img src="/icones/instagram.svg" alt="icone-instagram" />
+                        <img src="/icones/face.svg" alt="icone-facebook" />
+                        <img src="/icones/wpp.svg" alt="icone-whatsapp" />
                     </div>
 
                     <div className="infos">
-                        <p>Politica de Privacidade</p>
+                        <p>Política de Privacidade</p>
                         <p>Central de Ajuda</p>
-                        <p>Atendimentoa ao Cliente</p>
-                        <p>Termos de uso</p>
+                        <p>Atendimento ao Cliente</p>
+                        <p>Termos de Uso</p>
                     </div>
+                </div>
+
+                <div className="copyright">
+                    <p>Copyright © 2024; Designed by <span>Jennifer & Kauã</span></p>
                 </div>
 
             </footer>
--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import './Home.css'
+
 export function Home() {
+    const [infoBanner, setInfoBanner] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/banner")
+            .then((response) => response.json())
+            .then((data) => setInfoBanner(data))
+    }, []);
+
+    console.log(infoBanner);
+
     return (
         <>
-            
+            <section>
+                {infoBanner.map((info) => (
+                    <div className="Banner-inicial" key={info.id}>
+
+                        <div className="textos-home">
+                            <h1>{info.titulo}</h1>
+                            <button>Reservar uma mesa</button>
+                        </div>
+
+                        <div className="img-banner">
+                            <img src={info.imgBanner} alt="" />
+                        </div>
+                    </div>
+                ))}
+            </section>
+
+
+            <main>
+                <div className="texto-menu">
+                    <h2>Nosso  Menu</h2>
+                    <p>
+                        Bem-vindo ao nosso oásis gastronômico! Aqui,
+                        cada prato é uma jornada de sabores, cuidadosamente
+                        preparados para encantar seu paladar.
+                    </p>
+                </div>
+            </main>
         </>
+
+
     );
 }
