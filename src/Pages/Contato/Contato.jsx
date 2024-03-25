e963456e34b5563c5b508fdecebc2b12243d795d
Jennifer <lopesdossantosjennifergabriely@gmail.com>
2024-03-24 23:00:21 -0300

    Criando Contato

--- src/Pages/Contato/Contato.jsx
+++ src/Pages/Contato/Contato.jsx
@@ -1,5 +1,44 @@
+import './Contato.css'
+
 export function Contato() {
     return (
-        <h1>oioi</h1>
+        <section className="Conteudo-Contato">
+            <h2>Conheça nossas lojas</h2>
+
+            <div className="lojas">
+
+                <div className="conteudoLoja">
+                    <img src="./icones/Local.svg" alt="" />
+
+                    <div className="text">
+
+                    <h3>Rua das flores</h3>
+                    <p>Número 31, Bairro  Jardim</p>
+                    </div>
+                </div>
+
+
+                <div className="conteudoLoja">
+                    <img src="./icones/Local.svg" alt="" />
+
+                    <div className="text">
+
+                    <h3>Rua das flores</h3>
+                    <p>Número 31, Bairro  Jardim</p>
+                    </div>
+                </div>
+
+                <div className="conteudoLoja">
+                    <img src="./icones/Local.svg" alt="" />
+
+                    <div className="text">
+
+                    <h3>Rua das flores</h3>
+                    <p>Número 31, Bairro  Jardim</p>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
     )
 }
