1d376d9d4468d7decfc5f6ccb022ac1c5d1e045d
JenniGabriely01 <141264193+JenniGabriely01@users.noreply.github.com>
2024-03-27 13:40:22 -0300

    Criando pag de login e terminando estilização

--- src/Componentes/Header/header.jsx
+++ src/Componentes/Header/header.jsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import './Header.css'
 
 export function Header() {
+    const location = useLocation();
+
+    // Função para verificar se a rota atual corresponde à rota fornecida
+    const isCurrentRoute = (route) => {
+        return location.pathname === route;
+    };
+
     return (
         <>
             <header className='header'>
@@ -11,39 +19,34 @@ export function Header() {
                 </div>
 
                 <div className="nav">
-
                     <ul>
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/" className={isCurrentRoute("/") ? "active" : ""}>Home</Link>
                         </li>
                     </ul>
-
                     <ul>
                         <li>
-                            <Link to="/Servicos">Servicos</Link>
+                            <Link to="/Servicos" className={isCurrentRoute("/Servicos") ? "active" : ""}>Servicos</Link>
                         </li>
                     </ul>
-
                     <ul>
                         <li>
-                            <Link to="/Contato">Contatos</Link>
+                            <Link to="/Contato" className={isCurrentRoute("/Contato") ? "active" : ""}>Contatos</Link>
                         </li>
                     </ul>
-
                     <ul>
                         <li>
-                            <Link to="/SobreNos">Sobre Nós</Link>
+                            <Link to="/SobreNos" className={isCurrentRoute("/SobreNos") ? "active" : ""}>Sobre Nós</Link>
                         </li>
-                    </ul> 
-
+                    </ul>
                 </div>
 
 
                 <div className="icones-esquerda">
                     <img src="./icones/pesq.svg" alt="icone Pesquisa" />
                     <img src="./icones/icone-notificacao.svg" alt="icone Pesquisa" />
-                    <a href="/Login"> 
-                    <button href="/Login">Login</button>
+                    <a href="/Login">
+                        <button href="/Login">Login</button>
                     </a>
                 </div>
             </header>
